740a0ffc6fd7d174a181fc86c15d5e3ae568e0b2
David Jenkins <david.nicholas.jenkins@gmail.com>
2017-10-10 14:03:12 -0400

    Updated. Now set to send SMS phone for notification in via CLI option

--- notifi.py
+++ notifi.py
@@ -1,5 +1,4 @@
 import click
-import dotenv
 import logging
 
 from os import environ
@@ -28,22 +27,33 @@ def text_notification(to_number, from_number, message):
     logger.debug("Debugging text message. Message SID: {}".format(msg.sid))
 
 
+def phone_to_us_national_format(phone):
+    if not phone.startswith('+1'):
+        return '+1%s' % phone
+
+    return phone
+
+
 @click.command()
 @click.option("--should-text/--should-not-text", default=False, help="Send notification via text?")
+@click.option("--sms-phone", default=None, help="Phone number to send SMS.")
 @click.option("--notifi-msg", default="", help="Notification message to send.")
-def notifi_handler(should_text, notifi_msg):
+def notifi_handler(should_text, sms_phone, notifi_msg):
     logger.info("Loading configuration")
     load_configuration()
 
-    if should_text:
-        logger.info('Sending text notification. Phone: {}, Msg: {}'.format(
-            environ.get('NOTIFICATION_NUMBER'), notifi_msg))
+    if should_text and sms_phone:
+        sms_phone = phone_to_us_national_format(sms_phone)
+        from_phone = phone_to_us_national_format(
+            environ.get('TWILIO_FROM_NUMBER'))
 
-        text_notification(
-            environ.get('NOTIFICATION_NUMBER'),
-            environ.get('TWILIO_FROM_NUMBER'),
-            notifi_msg
-        )
+        logger.info('Sending text notification. Phone: {}, Msg: {}'.format(
+            sms_phone, notifi_msg))
+
+        text_notification(sms_phone, from_phone, notifi_msg)
+    elif should_text and sms_phone == None:
+        logger.error("Can't send notification. No --sms-number supplied. Msg: {}".format(
+            notifi_msg))
 
 
 if __name__ == "__main__":
